67bc081c2f6d11c3b7fe538b5050d39ac155802f
Piotr Romaniuk <piotr.romaniuk1990@gmail.com>
2022-05-05 19:54:14 +0200

    Added types of variables

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,7 +1,21 @@
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+        //liczby całkowite
+        System.out.println(10);
+
+        //liczby zmiennoprzecinkowe
+        System.out.println(10.5);
+
+        //text
+        System.out.println("text w Javie");
+
+        //znak
+        System.out.println('a');
+
+        //boolean
+        System.out.println(true);
+        System.out.println(false);
+    }
 }
