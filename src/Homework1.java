bc8b7606b9afed4e3014785a2ef6492b9730c513
Piotr Romaniuk <piotr.romaniuk1990@gmail.com>
2022-05-16 18:41:01 +0200

    Formatting code in homework1

--- src/Homework1.java
+++ src/Homework1.java
@@ -6,7 +6,7 @@ public class Homework1 {
         int numThree = 18;
         int numFour = 4;
 
-        if((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0 ) {
+        if ((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0) {
             System.out.println("Wszystkie liczby są parzyste");
         } else {
             System.out.println("Liczby nie są parzyste");
