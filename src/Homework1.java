31e172fdbdff3eddd9d39c61e8ad2c106d607442
Piotr Romaniuk <piotr.romaniuk1990@gmail.com>
2022-05-16 18:34:01 +0200

    Added homework 1

---
+++ src/Homework1.java
@@ -0,0 +1,15 @@
+public class Homework1 {
+    public static void main(String[] args) {
+
+        int numOne = 37;
+        int numTwo = 545;
+        int numThree = 18;
+        int numFour = 4;
+
+        if((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0 ) {
+            System.out.println("Wszystkie liczby są parzyste");
+        } else {
+            System.out.println("Liczby nie są parzyste");
+        }
+    }
+}
